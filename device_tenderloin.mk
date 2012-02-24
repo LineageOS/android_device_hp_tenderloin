f38601597ae9ea037d8a54de89ab7de1d9ae7f55
Dalingrin <dalingrin@gmail.com>
2012-02-24 00:40:28 -0600

    Add basic device settings app
    Only setting implemented so far is changing touchscreen mode.
    
    Changes needed to ts_srv_set:
    Dependent on ts_srv_set returning 0 for success or non 0 for error.
    ts_srv_set should be able to get current touchscreen mode. ts_srv_set
    should return F for finger or S for stylus.
    
    Change-Id: I066ddc79a365f458ec014ef2d10ad38103bdc9ce

--- device_tenderloin.mk
+++ device_tenderloin.mk
@@ -92,7 +92,8 @@ PRODUCT_PACKAGES += \
     bcattach \
     serial \
     com.android.future.usb.accessory \
-    rebootcmd
+    rebootcmd \
+    TenderloinParts
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
