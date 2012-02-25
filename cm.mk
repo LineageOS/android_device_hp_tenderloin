2c90f6be8cb7fd6447a2c18490b7ae9746a1f46c
eyeballer <eyeballer@teamw.in>
2012-02-25 07:06:10 +0000

    Update BUILD_ID to IML74K - fixes market compatibility for Chrome Beta

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := Touchpad
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_ID=GWK74 BUILD_FINGERPRINT=google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys" BUILD_NUMBER=189904
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_ID=IML74K BUILD_FINGERPRINT=google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys" BUILD_NUMBER=189904
 
 PRODUCT_NAME := cm_tenderloin
 PRODUCT_DEVICE := tenderloin
