bf72369042efcfe7f0f2f82bb0cb98ab5a772ea8
eyeballer <eyeballer@teamw.in>
2012-03-18 01:20:52 -0400

    Set bootanimation.zip resolution
    Per: http://review.cyanogenmod.com/#change,13579

--- cm.mk
+++ cm.mk
@@ -3,6 +3,7 @@ $(call inherit-product, device/hp/tenderloin/device_tenderloin.mk)
 PRODUCT_RELEASE_NAME := Touchpad
 
 # Inherit some common CM stuff.
+TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=touchpad BUILD_ID=IML74K BUILD_FINGERPRINT=hp/hp_tenderloin/tenderloin:4.0.3/IML74K/223971:user/release-keys PRIVATE_BUILD_DESC="tenderloin-user 4.0.3 IML74K 223971 release-keys" BUILD_NUMBER=189904
