baa3f4518219b94b5e43522e16c00f4422518e4e
Tomasz Rostanski <rostan@podkowa.net>
2012-05-24 16:02:30 +0200

    TouchPad: Camera HAL
    This is the TouchPad port of msm7x30 HAL
    (http://review.cyanogenmod.com/#/c/13650/)
    
    Requires:
    http://review.cyanogenmod.com/#/c/13285/
    http://review.cyanogenmod.com/#/c/16102/
    http://review.cyanogenmod.com/#/c/16120/
    http://review.cyanogenmod.com/#/c/16121/
    http://review.cyanogenmod.com/#/c/16122/
    http://review.cyanogenmod.com/#/c/24703/
    
    Change-Id: I5149bc51ff34246e7c7cb44c499e0cf7bdd3cbd6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,9 @@ BOARD_FIRST_CAMERA_FRONT_FACING := true
 BOARD_CAMERA_USE_ENCODEDATA := true
 
 BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
-USE_CAMERA_STUB := true
+BOARD_USES_HTC_CAMERA := true
+BOARD_USE_FROYO_LIBCAMERA := true
+USE_CAMERA_STUB := false
 
 # tenderloin- these kernel settings are temporary to complete build
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom
